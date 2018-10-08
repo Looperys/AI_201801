0302b99bd1a3fa694bf472b69054fde8e0502ab7
Wind2009 <wind.jackey@qq.com>
2018-10-08 17:35:11 +0800

    Data update

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -5,6 +5,12 @@ using namespace std;
 int main() {
 	cout << "0" << endl;
 	// file test
+	ifstream train_file;
+	train_file.open("Input/2/testData.txt");
+	if (train_file.is_open()) {
+		cout << "Open!" << endl;
+	}
+	train_file.close();
 	system("pause");
 	return 0;
 }
